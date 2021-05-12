0b7e1d07753b0b00fc2f0736cf740ef5ab386b6c
aaaafr <anastasia.frassini@icloud.com>
2021-05-12 16:12:19 +0200

    COMPLETE

---
+++ snack-3/main.js
@@ -0,0 +1,51 @@
+// Snack 3:
+// Scrivere una funzione che accetti tre argomenti,
+//  un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array
+//  con i valori che hanno la posizione compresa 
+// tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+
+
+// FOR EACH
+
+// const lista = ["a", "b", "c", "d"];
+// const a = 1;
+// const b = 3;
+
+// function filterArray (lista, a, b){
+//     var newArray = [];
+//     lista.forEach((element,index) => {
+//         console.log(element);
+//         console.log(index);
+//         if((a <= index) && (b >= index)){
+//             newArray.push(element);
+//         }        
+//     });
+//     console.log(newArray);
+
+// }
+
+// filterArray(lista,a,b);
+
+
+
+// FILTER
+
+// const lista = ["a", "b", "c", "d"];
+// const a = 1;
+// const b = 3;
+
+// function filterArray (lista, a, b){
+//     const newArray = lista.filter((element,index) => {
+//         return ((a <= index) && (b >= index))
+        
+//     });
+//     console.log(newArray);
+
+// }
+
+// filterArray(lista,a,b);
+
+
+
